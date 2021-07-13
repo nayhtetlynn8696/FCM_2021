f853b068d2cb9c578c9d91e5229e9cf6adec200b
Nay Htet Lynn <57884748+NayHtetLynn-Git@users.noreply.github.com>
2021-07-13 17:02:53 +0630

    Add Notification Detail

--- app/src/main/java/com/linn/solution/fcm_2021/FcmMessageReceiverService.kt
+++ app/src/main/java/com/linn/solution/fcm_2021/FcmMessageReceiverService.kt
@@ -1,90 +1,61 @@
 package com.linn.solution.fcm_2021
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.media.RingtoneManager
-import android.os.Build
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.*
 
-class FcmMessageReceiverService:FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage : RemoteMessage) {
+class FcmMessageReceiverService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-
-        if (remoteMessage.data.isEmpty()){
-            showNotification(remoteMessage.notification!!.title.toString(),
-                remoteMessage.notification!!.body.toString()
-            )
-        }else{
-            showNotification(remoteMessage.data as HashMap<String, String>)
-        }
+        addNotification(remoteMessage)
     }
 
-    private fun showNotification(data: HashMap<String, String>){
-        val title= data["title"].toString()
-        val body= data["message"].toString()
-        val id=data["userId"].toString()
-        Log.d("LogData",id)
-        val channel_id=getString(R.string.app_name)
-
-        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val builder = NotificationCompat.Builder(applicationContext, channel_id)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setSound(uri)
-            .setAutoCancel(true)
-            .setWhen(System.currentTimeMillis())
-            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
-            .setOnlyAlertOnce(true)
-            .setContentTitle(title)
-            .setContentText(body)
-
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                channel_id,
-                "Notification",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationChannel.setSound(uri, null)
-            notificationManager.createNotificationChannel(notificationChannel)
+    private fun addNotification(remoteMessage: RemoteMessage) {
+        val notifyIntent = Intent(remoteMessage.notification!!.clickAction).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
-        notificationManager.notify(Random().nextInt(), builder.build())
-    }
 
-    private fun showNotification(title:String, body:String){
-        val channel_id=getString(R.string.app_name)
+        val notifyPendingIntent = PendingIntent.getActivity(
+            this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
 
-        val uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val builder = NotificationCompat.Builder(applicationContext, channel_id)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setSound(uri)
-            .setAutoCancel(true)
-            .setWhen(System.currentTimeMillis())
-            .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
-            .setOnlyAlertOnce(true)
-            .setContentTitle(title)
-            .setContentText(body)
-
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                channel_id,
-                "Notification",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationChannel.setSound(uri, null)
-            notificationManager.createNotificationChannel(notificationChannel)
+        val builder = NotificationCompat.Builder(this, getString(R.string.app_name)).apply {
+            setContentIntent(notifyPendingIntent)
         }
-        notificationManager.notify(Random().nextInt(), builder.build())
+        with(NotificationManagerCompat.from(this)) {
+            notify(Random().nextInt(), builder.build())
+        }
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
-        Log.d("Tokennnnnnn", token)
+        Log.d("LogData",token)
+    }
+
+    fun getBitmap(imageUrl: String?): Bitmap? {
+        return try {
+            val url = URL(imageUrl)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input: InputStream = connection.inputStream
+            BitmapFactory.decodeStream(input)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 }
--- app/src/main/java/com/linn/solution/fcm_2021/MainActivity.kt
+++ app/src/main/java/com/linn/solution/fcm_2021/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.linn.solution.fcm_2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.freshchat.consumer.sdk.Freshchat
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         FirebaseMessaging.getInstance().isAutoInitEnabled=true
 
+        FirebaseMessaging.getInstance().subscribeToTopic("news")
         FirebaseMessaging.getInstance().token
                 .addOnCompleteListener(OnCompleteListener { task ->
                     if (!task.isSuccessful) {
@@ -23,7 +32,64 @@ class MainActivity : AppCompatActivity() {
                     // Get new FCM registration token
                     val token = task.result
 
+                    Freshchat.getInstance(this).setPushRegistrationToken(token!!)
                     Log.d("LogData1", "Token==$token")
                 })
+
+
+        findViewById<MaterialTextView>(R.id.tv_view).setOnClickListener {
+            showNoti()
+        }
+
+    }
+
+    private fun showNoti(){
+        val notifyIntent = Intent(this, ResultActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val notifyPendingIntent = PendingIntent.getActivity(
+            this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val builder = NotificationCompat.Builder(this, "CHANNEL_ID").apply {
+            setContentTitle("Notifications Example")
+            setContentText("This is a test notification")
+            setSmallIcon(R.mipmap.ic_launcher)
+            setContentIntent(notifyPendingIntent)
+        }
+        val notificationIntent = Intent(this, ResultActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        //notification message will get at NotificationView
+        notificationIntent.putExtra("message", "This is a notification message")
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder.setContentIntent(pendingIntent)
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+    }
+
+    private fun addNotification() {
+        val builder = NotificationCompat.Builder(this,getString(R.string.app_name))
+            .setSmallIcon(R.mipmap.ic_launcher) //set icon for notification
+            .setContentTitle("Notifications Example") //set title of notification
+            .setContentText("This is a notification message") //this is notification message
+            .setAutoCancel(true) // makes auto cancel of notification
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT) //set priority of notification
+        val notificationIntent = Intent(this, ResultActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        //notification message will get at NotificationView
+        notificationIntent.putExtra("message", "This is a notification message")
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder.setContentIntent(pendingIntent)
+
+        // Add as notification
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(0, builder.build())
     }
 }
